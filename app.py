6315abf6b64735d508994b7a6534ce2e0fe070dd
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-08-07 22:28:41 +0500

    added routes

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+@app.route('/contact')
+def shop():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 app.run(host='0.0.0.0', port=port)
