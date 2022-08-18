9803bf7e90f53ff39d6e5f476111c94d8c81017b
WarzeEgg <contact@warze.org>
2022-08-18 16:04:56 +0200

    Added profile

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+# Importing all of the dependencies
 from flask import Flask, render_template
 import os
 
+# This is the port number, defaults to 5000 if no environment variable is found
 port = int(os.getenv('PORT', 5000))
 
+# Creating the app, it will be referenced in all of the routes and when starting the server
 app = Flask(__name__)
 
+# The homepage
 @app.route('/')
-def index():
+def homepage():
     return render_template('index.html')
 
 @app.route('/art')
@@ -32,8 +36,19 @@ def art():
 def about():
     return render_template('about.html')
 
+@app.route('/profile/<username>')
+def profile(username):
+    if username == 'Warze':
+        coins = 500
+    else:
+        coins = 0
+    return render_template('profile.html', username = username, coins = coins)
+
+# The 404 page
 @app.errorhandler(404)
 def errorhandler(e):
+    print(e)
     return render_template('404.html')
 
+# Start hosting the server
 app.run(host='0.0.0.0', port=port, debug=True)
